5eda68516e888372397823343e22f333cc19c662
BenjaminB <haskellishaskell@gmail.com>
2016-02-04 09:40:45 +0100

    added replaceAt

--- chineseCheckers.hs
+++ chineseCheckers.hs
@@ -1,3 +1,6 @@
+import Data.Sequence
+import Data.Foldable
+
 type Table = [[Square]]
 type Pgoal = Int
 data Square = Empty Pgoal | Piece Pgoal
@@ -18,14 +21,20 @@ place t (x1,y1) (x2,y2) = case (getSquare t (x2,y2)) of
 place' :: Table -> Square -> (Int,Int) -> Table
 place' t s (x,y) = undefined
 
+
 insert2Dlist :: (Int,Int) -> a -> [[a]] -> [[a]]
-insert2Dlist (x,y) a xs =  undefined 
+--insert2Dlist (x,y) a xs = (fst(Prelude.splitAt (y+1) xs)) ++ [replaceAt x a (Prelude.concat (snd(Prelude.splitAt (y+1) xs)))] ++ (snd(Prelude.splitAt (y+1) xs))
 
+insert2Dlist (x,y) a xs = (fst(Prelude.splitAt (y+1) xs)) ++ replaceAt x a head . snd(Prelude.splitAt (y+1) xs) ++ (snd(Prelude.splitAt (y+1) xs))
 
-move :: Table -> (Int, Int) -> (Int, Int) -> Table 
+move :: Table -> (Int, Int) -> (Int, Int) -> Table
 move t (x1,y1) (x2, y2) = undefined
 
 getSquare :: Table -> (Int,Int) -> Square
 getSquare t (x,y) = (t !! y) !! x
+
+replaceAt :: Int -> a -> [a] -> [a]
+replaceAt i a xs = toList .  update i a $ fromList xs
+                        
 
 gameLoop = undefined
