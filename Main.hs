4c4bd3e290e9b32199da915886aba3407a4d31e3
Kalior <kalioragus@gmail.com>
2016-02-13 22:30:12 +0100

    Add create game in client (will crash beacuse of missing html elements)

--- Main.hs
+++ Main.hs
@@ -4,15 +4,30 @@ import Haste.App
 import Haste.App.Standalone
 import Lobby
 import qualified Control.Concurrent as CC
+import Haste.Events
+import Haste.DOM
 
 main :: IO ()
 main = runStandaloneApp $ do
       playersList <- liftServerIO $ CC.newMVar []
+      gamesList <- liftServerIO $ CC.newMVar []
 
       handshake <- remote $ srvHandshake playersList
+      createGame <- remote $ srvCreateGame gamesList playersList
       onSessionEnd $ srvCloseConnection playersList
 
       runClient $ do
-        name <- prompt "Hello! Please enter your name:"
-        onServer $ handshake <.> name
-        return ()
+
+         name <- prompt "Hello! Please enter your name:"
+         onServer $ handshake <.> name
+
+         _ <- ($)
+            withElems ["createGamebtn"] $ \[createGamebtn] ->
+               onEvent createGamebtn Click $ \(MouseData _ mb _) ->
+                  case mb of
+                     Just MouseLeft -> do
+                        onServer createGame
+                     _ -> return ()
+
+
+         return ()
