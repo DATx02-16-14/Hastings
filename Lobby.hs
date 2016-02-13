bf20820b40d2529e3cda858dd478eddbba68dcd5
Kalior <kalioragus@gmail.com>
2016-02-13 22:23:04 +0100

    Add method for creating a game on the server

--- Lobby.hs
+++ Lobby.hs
@@ -1,8 +1,9 @@
 module Lobby
   where
 import Haste.App
-
 import qualified Control.Concurrent as CC
+import Data.List
+import Haste.DOM
 
 type Name = String
 type Player = (SessionID, Name)
@@ -23,3 +24,15 @@ srvCloseConnection remotePlayers sid = do
   players <- remotePlayers
   liftIO $ CC.modifyMVar_ players $ \ps ->
     return $ filter ((sid /=) . fst) ps
+
+srvCreateGame :: Server GamesList -> Server PlayerList -> Server ()
+srvCreateGame remoteGames remotePlayers = do
+   players <- remotePlayers
+   games <- remoteGames
+   sid <- getSessionID
+   liftIO $ CC.modifyMVar_ games $ \gs -> do
+      playerList <- liftIO $ CC.readMVar players
+      let maybePlayer = find (\p -> fst p == sid) playerList
+      case maybePlayer of
+         Just p -> return $ ("string", [p]) : gs
+         Nothing -> return gs
