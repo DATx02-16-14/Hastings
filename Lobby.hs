10a2aa679e0e48e1581669b16d3d84e7d21d50fb
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-02-15 15:27:57 +0100

    Display list of created games in client

--- Lobby.hs
+++ Lobby.hs
@@ -23,3 +23,9 @@ srvCloseConnection remotePlayers sid = do
   players <- remotePlayers
   liftIO $ CC.modifyMVar_ players $ \ps ->
     return $ filter ((sid /=) . fst) ps
+
+srvGetGamesList :: Server GamesList -> Server [String]
+srvGetGamesList remoteGames = do
+  gameList <- remoteGames >>=  liftIO . CC.readMVar
+  return $ map fst gameList
+
--- Main.hs
+++ Main.hs
@@ -2,17 +2,30 @@ module Main
     where
 import Haste.App
 import Haste.App.Standalone
+import Haste.DOM
 import Lobby
 import qualified Control.Concurrent as CC
 
 main :: IO ()
 main = runStandaloneApp $ do
       playersList <- liftServerIO $ CC.newMVar []
+      gamesList <- liftServerIO $ CC.newMVar []
 
       handshake <- remote $ srvHandshake playersList
+      getGamesList <- remote $ srvGetGamesList gamesList
       onSessionEnd $ srvCloseConnection playersList
 
       runClient $ do
         name <- prompt "Hello! Please enter your name:"
         onServer $ handshake <.> name
+
+        gameList <- onServer getGamesList
+        mapM_ addGameToDOM gameList
+
         return ()
+
+
+addGameToDOM :: String -> Client ()
+addGameToDOM gameName = do
+   textElem <- newTextElem gameName
+   appendChild documentBody textElem
