96ac7cc9cd5fecaaf4420c44b03d869ef7f21c3b
Kalior <kalioragus@gmail.com>
2016-02-17 11:34:14 +0100

    Add method to create DOM for a lobby game

--- Lobby.hs
+++ Lobby.hs
@@ -57,6 +57,36 @@ createLobbyDOM = do
    appendChild parentDiv createGamebtn
    appendChild documentBody parentDiv
 
+createGameDOM :: (String,[String]) -> IO ()
+createGameDOM (gameId,ps) = do
+    parentDiv <- newElem "div" `with`
+        [
+            prop "id" =: "lobbyGame"
+        ]
+    nameOfGame <- newTextElem gameId
+    header <- newElem "h1" `with`
+        [
+            style "text-align" =: "center",
+            style "margin-left" =: "auto",
+            style "margin-right" =: "auto"
+        ]
+    appendChild header nameOfGame
+    appendChild parentDiv header
+    list <- newElem "div" `with`
+        [
+            prop "id" =: "playerList"
+        ]
+    listhead <- newTextElem "Players: "
+    appendChild list listhead
+    mapM_ (\p -> do
+                name <- newTextElem $ p ++ " "
+                appendChild list name
+            ) ps
+    appendChild parentDiv list
+    appendChild documentBody parentDiv
+
+createGameDOMWithGame :: LobbyGame -> IO ()
+createGameDOMWithGame lobbyGame = createGameDOM (fst lobbyGame, map snd $ snd lobbyGame)
 
 deleteLobbyDOM :: IO ()
 deleteLobbyDOM =
