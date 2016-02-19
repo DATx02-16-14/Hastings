5c6e9fefe1410deac95697b554714f05709b7c9a
BenjaminB <haskellishaskell@gmail.com>
2016-02-19 11:08:20 +0100

    Update to use stack.

--- src/ChineseGraphics.hs
+++ src/ChineseGraphics.hs
@@ -1,8 +1,10 @@
+module ChineaseGraphics
+	where
 import Haste
 import Haste.DOM
 import Haste.Graphics.Canvas
 import Haste.Events
-import ChineseCheckers
+--import ChineseCheckers
 import qualified Control.Concurrent as CC
 import Table
 
@@ -15,8 +17,8 @@ starOfDavid space size = do
             ((24.5+1+5)*space+25*size, size*5+space*(5+5-2)), ((21+1+5)*space+21*size, size*9+space*(9+5-2)),
             ((24.5+1+5)*space+25*size, size*13+space*(13+5-2)), ((24+5+1)*space+24*size, size*14+space*(14+5-2)),
             ((16+5+1)*space+16*size, size*14+space*(14+5-2)), ((12+5+1)*space+12*size, size*18+space*(18+5-2)),
-            ((12+5-1)*space+12*size, size*18+space*(18+5-2)), ((8+5-1)*space+8*size, size*13.5+space*(13.5+5-2)),
-            ((8+5-1)*space+8*size, size*13.5+space*(13.5+5-2)), ((5-1)*space+0*size, size*13.5+space*(13.5+5-2)), 
+            ((12+5-1)*space+12*size, size*18+space*(18+5-2)), ((8+5-1)*space+8*size, size*14+space*(14+5-2)),
+            ((8+5-1)*space+8*size, size*14+space*(14+5-2)), ((5-1)*space+0*size, size*14+space*(14+5-2)), 
             ((3.5-1)*space+0*size, size*13+space*(13+5-2)), ((3+5-1)*space+3*size, size*9+space*(9+5-2)),
             ((3.5-1)*space+0*size, size*5+space*(5+5-2)), ((5-1)*space+0*size, size*4+space*(4+5-2)),
             ((8+5-1)*space+8*size, size*4+space*(4+5-2)), ((12+5-1)*space+12*size, space*(5-2))] 
@@ -32,11 +34,11 @@ drawSquare space size (Square Empty col (x,y)) = do
 
 
 initTable' :: Picture ()
-initTable' = sequence_ $ map (drawSquare 15 20) startTable
+initTable' = sequence_ $ map (drawSquare 15 25) startTable
 
 starOfDavidInABox :: Picture ()
 starOfDavidInABox = do
-    fill $ starOfDavid 15 20
+    fill $ starOfDavid 15 25
     initTable'
 
 
@@ -57,7 +59,7 @@ drawGame = undefined
 --main :: IO ()
 main = do
     stateOfGame <- CC.newEmptyMVar
-    CC.putMVar stateOfGame $ initGame ["Pelle", "Lasse"]
+    --CC.putMVar stateOfGame $ initGame ["Pelle", "Lasse"]
     canvas <- mkCanvas 1900 800
     appendChild documentBody canvas
     canvas2 <- mkCanvas 500 500
