b27e6c6e5c45553c572fda46fc210e5ed230ce9b
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-03-08 10:33:20 +0100

    Refactor common code test/Utils.hs

--- test/Utils.hs
+++ test/Utils.hs
@@ -8,22 +8,21 @@ import Hastings.Utils
 --Test that checks that exactly one element in the list has been updated and that the updated value is correct
 prop_updateLookup_correctUpdate :: Int -> [(Int, Int)] -> Property
 prop_updateLookup_correctUpdate element list = isJust value ==>
-     length resultDiff == 1 && snd (head resultDiff) == updateFun (fromJust value)
+     length resultDiff == 1 && snd (head resultDiff) == updateFunction (fromJust value)
 
   where
-    result = updateLookup updateFun element list
+    result = updateLookup updateFunction element list
     resultDiff = result \\ list
-    updateFun v = v + 1
     value = lookup element list
 
 --Test that checks that the order of the updated list is preserved.
 prop_updateLookup_correctOrder :: Int -> [(Int, Int)] -> Property
-prop_updateLookup_correctOrder element list = isJust value ==>
+prop_updateLookup_correctOrder element list = isJust (lookup element list) ==>
     length list == length result && length filteredList == 1
 
     where
-      result = updateLookup updateFun element list
-      zipList = zip result list
-      filteredList = filter (uncurry (/=)) zipList
-      updateFun v = v + 1
-      value = lookup element list
+      result = updateLookup updateFunction element list
+      filteredList = filter (uncurry (/=)) $ zip result list
+
+updateFunction :: Int -> Int
+updateFunction value = value + 1
