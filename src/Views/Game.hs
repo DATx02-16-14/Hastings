7af311dbe501613011f8c20491c72bb7cd1a72f1
Kalior <kalioragus@gmail.com>
2016-04-01 11:27:47 +0200

    Make changeGameName and updateMaxNumberPlayers use generic function

--- src/Views/Game.hs
+++ src/Views/Game.hs
@@ -58,31 +58,7 @@ createGameDOM api gapi = do
 -- |Creates the DOM for chaning the name of a game.
 -- |It includes an input field and a button.
 createGameChangeNameDOM :: LobbyAPI -> Client ()
-createGameChangeNameDOM api = do
-  gameNameDiv <- createDiv [("id", "changeGameName")]
-  gameNameText <- newTextElem "Change game name"
-  gameNameField <- newElem "input" `with`
-    [
-      attr "type" =: "text",
-      attr "id" =: "gameNameField"
-    ]
-  gameNameButton <- newElem "button" `with`
-    [
-      attr "id" =: "gameNameBtn"
-    ]
-  gameNameBtnText <- newTextElem "Change"
-  appendChild gameNameButton gameNameBtnText
-
-  addChildrenToParent' gameNameDiv [gameNameText, gameNameField, gameNameButton]
-
-  addChildrenToRightColumn [gameNameDiv]
-
-  onEvent gameNameField KeyPress $ \13 -> gameUpdateFunction
-
-  clickEventString "gameNameBtn" gameUpdateFunction
-
-  return ()
-
+createGameChangeNameDOM api = createInputFieldWithButton "gameName" "Change game name" gameUpdateFunction
   where
     gameUpdateFunction =
       withElem "gameNameField" $ \field -> do
@@ -97,32 +73,8 @@ createGameChangeNameDOM api = do
 -- |Creates DOM for the updatin the maximum number of players in a game
 -- |Contains an input field and a button. Is placed in the right sidebar
 createUpdateMaxNumberPlayersDOM :: LobbyAPI -> GameAPI-> Client ()
-createUpdateMaxNumberPlayersDOM api gapi = do
-  maxNumberDiv <- createDiv [("id","maxNumberDiv")]
-
-  maxNumberText <- newTextElem "Change maximum number of players"
-  maxNumberField <- newElem "input" `with`
-    [
-      attr "type" =: "text",
-      attr "id" =: "maxNumberField"
-    ]
-  maxNumberButton <- newElem "button" `with`
-    [
-      attr "id" =: "maxNumberBtn"
-    ]
-  maxNumberBtnText <- newTextElem "Change"
-
-  appendChild maxNumberButton maxNumberBtnText
-  appendChild maxNumberDiv maxNumberText
-  appendChild maxNumberDiv maxNumberField
-  appendChild maxNumberDiv maxNumberButton
-  addChildrenToRightColumn [maxNumberDiv]
-
-  onEvent maxNumberField KeyPress $ \13 -> maxNumberUpdateFunction
-
-  clickEventString "maxNumberBtn" maxNumberUpdateFunction
-  return ()
-
+createUpdateMaxNumberPlayersDOM api gapi =
+  createInputFieldWithButton "maxNumber" "Change maximum number of players" maxNumberUpdateFunction
   where
     maxNumberUpdateFunction =
       withElem "maxNumberField" $ \field -> do
