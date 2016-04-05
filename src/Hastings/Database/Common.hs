49ba656ad8fab4f843fe6961bf91dd259cb5e10e
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-04-05 15:40:27 +0200

    Add common database functions

---
+++ src/Hastings/Database/Common.hs
@@ -0,0 +1,24 @@
+module Hastings.Database.Common
+   where
+
+
+import qualified Database.Persist
+import qualified Database.Persist.MySQL as MySQL
+import Database.Esqueleto
+import Control.Monad.Logger
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Reader
+
+import Hastings.Database.Fields
+
+-- |Run database migration, creating all relevant tables.
+migrateDatabase :: IO ()
+migrateDatabase = runDB $ runMigration migrateAll
+
+-- |Helper function that should be called before running a query on the database.
+--
+-- @
+-- saveCar = runDB $ insert $ Car \"Volvo\"
+-- @
+runDB :: SqlPersistT (NoLoggingT IO) a -> IO a
+runDB = runNoLoggingT . MySQL.withMySQLConn MySQL.defaultConnectInfo . runSqlConn
