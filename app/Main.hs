170583819433f1b2c21df3996515b3badee5779d
André Samuelsson <andresamuelsson94@gmail.com>
2016-04-06 19:57:50 +0200

    Revert "Host configuration extracted to config file"
    This reverts commit e567fd144e5a78264f6816a56e29db3e07731c1b.

--- app/Main.hs
+++ app/Main.hs
@@ -20,25 +20,13 @@ import LobbyServer
 
 -- |Main method and entry point for the program
 main :: IO ()
-main = do
-  config <- readFile "config.conf"
-  let conf = getConfig config
-  print $ cfgHost conf
-  print $ cfgPort conf
-  runApp (getConfig . head $ words config) $ do
+main = runApp defaultConfig $ do
+  playersList <- liftServerIO $ CC.newMVar []
+  gamesList <- liftServerIO $ CC.newMVar []
+  chatList <- liftServerIO $ CC.newMVar []
 
-    playersList <- liftServerIO $ CC.newMVar []
-    gamesList <- liftServerIO $ CC.newMVar []
-    chatList <- liftServerIO $ CC.newMVar []
+  let serverState = (playersList, gamesList, chatList)
 
-    let serverState = (playersList, gamesList, chatList)
-
-    onSessionEnd $ disconnect(serverState)
-    api <- newLobbyAPI (playersList, gamesList, chatList)
-    runClient $ clientMain api newGameAPI
-
--- | The config used by the clients to connect to the server. i.e. where the server is running.
-getConfig :: String -> AppCfg
-getConfig config = do
-  let (h,(_:t)) = break (==':') config
-  mkConfig h (read t :: Int)
+  onSessionEnd $ disconnect(serverState)
+  api <- newLobbyAPI (playersList, gamesList, chatList)
+  runClient $ clientMain api newGameAPI
