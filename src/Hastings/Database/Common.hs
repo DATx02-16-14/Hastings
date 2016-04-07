830de6919496483c017863c1306c19582717f937
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-04-07 17:02:25 +0200

    Update documentation for runDB

--- src/Hastings/Database/Common.hs
+++ src/Hastings/Database/Common.hs
@@ -13,7 +13,15 @@ migrateDatabase :: IO ()
 migrateDatabase = runDB $ runMigration migrateAll
 
 -- |Helper function that should be called before running a query on the database.
+--  It's primary purpose is running the proper prerequisites to be able to excecute an SQL query on the database.
+--  It connects to a database with the following connection information
+-- * Server on localhost
+-- * User root
+-- * No password
+-- * Database test
+-- * Character set utf8
 --
+-- The following is an example on how you would use this function when inserting a car with the brand \"Volvo\" to the database.
 -- @
 -- saveCar = runDB $ insert $ Car \"Volvo\"
 -- @
