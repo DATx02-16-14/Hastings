0d34f78e907a1b773944e240c83055700abd50ed
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-04-07 16:03:55 +0200

    Fix imports in Player.hs

--- src/Hastings/Database/Player.hs
+++ src/Hastings/Database/Player.hs
@@ -2,33 +2,33 @@
 module Hastings.Database.Player
    where
 
-import Hastings.Database.Common
+import Hastings.Database.Common (runDB)
 import Hastings.Database.Fields
 
-import Database.Esqueleto
+import qualified Database.Esqueleto as Esql
 import Data.Word (Word64)
 import Data.Maybe (listToMaybe)
 
 
 -- |Save a player to the database.
 savePlayer :: String -- ^The name of the player to save.
-           -> IO (Key Player)
-savePlayer name = runDB $ insert $ Player name
+           -> IO (Esql.Key Player)
+savePlayer name = runDB $ Esql.insert $ Player name
 
 -- |Delete a player from the database.
 deletePlayer :: String -- ^The username of the player to delete.
              -> IO ()
-deletePlayer userName = runDB $ deleteBy $ UniqueUsername userName
+deletePlayer userName = runDB $ Esql.deleteBy $ UniqueUsername userName
 
 -- |Retrieve a player from the database by their username.
 retrievePlayerbyUsername :: String -- ^The username of the player to retrieve.
-                         -> IO (Maybe (Entity Player))
-retrievePlayerbyUsername name = runDB $ getBy $ UniqueUsername name
+                         -> IO (Maybe (Esql.Entity Player))
+retrievePlayerbyUsername name = runDB $ Esql.getBy $ UniqueUsername name
 
 -- |Change the username of a player.
 changeUserName :: String -- ^The old username.
                -> String -- ^The new username.
-               -> IO (Key Player)
+               -> IO (Esql.Key Player)
 changeUserName oldName newName = do
   deletePlayer oldName
   savePlayer newName
@@ -37,15 +37,15 @@ changeUserName oldName newName = do
 --  Creates a new player with the specified username if the player doesn't exist.
 saveOnlinePlayer :: String -- ^The name of the player to save.
                  -> Word64 -- ^The sessionID of the player to save.
-                 -> IO (Key OnlinePlayer)
+                 -> IO (Esql.Key OnlinePlayer)
 saveOnlinePlayer name sessionID = do
   player <- retrievePlayerbyUsername name
   case player of
-    Just entity -> saveOnlinePlayer' sessionID (entityKey entity)
+    Just entity -> saveOnlinePlayer' sessionID (Esql.entityKey entity)
     _           -> savePlayer name >>= saveOnlinePlayer' sessionID
 
     where
-      saveOnlinePlayer' sessionID key = runDB $ insert $ OnlinePlayer key sessionID
+      saveOnlinePlayer' sessionID key = runDB $ Esql.insert $ OnlinePlayer key sessionID
 
 
 -- |Retrieve an online player from the database.
@@ -57,15 +57,15 @@ saveOnlinePlayer name sessionID = do
 -- WHERE OnlinePlayer.Player == Player.Id
 -- @
 retrieveOnlinePlayer :: Word64 -- ^The sessionID of the player to retrieve.
-                     -> IO (Maybe (Entity Player))
+                     -> IO (Maybe (Esql.Entity Player))
 retrieveOnlinePlayer sessionID = runDB $ do
-  playerList <- select $
-       from $ \(b, p) -> do
-         where_ (b ^. OnlinePlayerPlayer ==. p ^. PlayerId)
+  playerList <- Esql.select $
+       Esql.from $ \(b, p) -> do
+         Esql.where_ (b Esql.^. OnlinePlayerPlayer Esql.==. p Esql.^. PlayerId)
          return p
   return $ listToMaybe playerList
 
 -- |Delete an online player from the database.
 deleteOnlinePlayer :: Word64 -- ^The sessionID of the player to delete.
                    -> IO ()
-deleteOnlinePlayer sessionID = runDB $ deleteBy $ UniqueSession sessionID
+deleteOnlinePlayer sessionID = runDB $ Esql.deleteBy $ UniqueSession sessionID
