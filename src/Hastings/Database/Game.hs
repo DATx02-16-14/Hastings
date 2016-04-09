2060fb13042e8dad4c2812a9ebb46941dbdaf255
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-04-09 15:36:07 +0200

    Add functions to retrieve and save players to games

--- src/Hastings/Database/Game.hs
+++ src/Hastings/Database/Game.hs
@@ -5,6 +5,8 @@ import Hastings.Database.Fields
 
 import qualified Database.Esqueleto as Esql
 
+import Haste.App (SessionID)
+
 -- |Retrieve a game from the database.
 retrieveGameByName :: String -- ^The name of the game to retrieve.
              -> IO (Maybe (Esql.Entity Game))
@@ -16,3 +18,22 @@ saveGame :: String -- ^The name of the game to save.
          -> IO (Esql.Key Game)
 saveGame name maxAmountOfPlayers = runDB $ Esql.insert $ Game name maxAmountOfPlayers
 
+-- |Add a player to a game
+addPlayerToGame :: SessionID -- ^The sessionID of the player.
+                -> Esql.Key Game -- ^The key of the game.
+                -> IO (Esql.Key PlayerInGame)
+addPlayerToGame sessionID gameKey = runDB $ Esql.insert $ PlayerInGame gameKey sessionID
+
+-- |Retrieves a players that are currently in a game.
+retrievePlayersInGame :: Esql.Key Game -- ^The key of the game.
+                      -> IO [Esql.Entity Player]
+retrievePlayersInGame gameKey = runDB $
+      Esql.select $ Esql.from $ \players -> do
+        (playerInGame, onlinePlayers) <- onlinePlayerInGame
+        Esql.where_ (onlinePlayers Esql.^. OnlinePlayerPlayer Esql.==. players Esql.^. PlayerId
+            Esql.&&. playerInGame Esql.^. PlayerInGameGame Esql.==. Esql.val gameKey)
+        return players
+  where
+    onlinePlayerInGame = Esql.from $ \(playerInGame `Esql.InnerJoin` onlinePlayers) -> do
+        Esql.on (playerInGame Esql.^. PlayerInGamePlayer Esql.==. onlinePlayers Esql.^. OnlinePlayerSessionID)
+        return (playerInGame, onlinePlayers)
