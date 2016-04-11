35e6739470134f86987055e1bd711b6216e8fecf
Kalior <kalioragus@gmail.com>
2016-04-11 17:16:30 +0200

    Add property for disconnect

--- test/Server/LobbyTest.hs
+++ test/Server/LobbyTest.hs
@@ -5,20 +5,41 @@ import Test.QuickCheck
 import Test.QuickCheck.Monadic
 import Control.Concurrent (readMVar, newMVar)
 
-import Server.Lobby
+import qualified Server.Lobby
 import ArbitraryLobbyTypes ()
 import LobbyTypes
 
 -- To run in ghci
 -- :l test/Server/LobbyTest.hs src/Hastings/ServerUtils.hs src/Hastings/Utils.hs src/LobbyTypes.hs test/ArbitraryLobbyTypes.hs src/Server/Lobby.hs src/Server/Game.hs src/Server/Chat.hs src/Hastings/Database/Player.hs
 
--- |Test for connect that makes sure that after running connect a sessionID
--- and name is found in the list.
+-- |Property for connect that makes sure that after running connect a ClientEntry with
+-- sessionID and name is found in the list.
 prop_connect :: Name -> [ClientEntry] -> Property
 prop_connect playerName list = monadicIO $ do
-  pre $ not $ null list
   sessionIDWord64 <- pick $ elements [184468..282345]
   mVar <- run $ newMVar list
-  run $ connect mVar playerName sessionIDWord64
+  run $ Server.Lobby.connect mVar playerName sessionIDWord64
   conList <- run $ readMVar mVar
   assert $ any (\c -> sessionIDWord64 == sessionID c && name c == playerName) conList
+
+-- |Property for disconnect that checks that after disconnecting, the cliententry
+-- is removed from all games and the list of clients.
+prop_disconnect :: Int  -- ^The index of player to remove
+                -> [ClientEntry] -> [LobbyGame] -> Property
+prop_disconnect i clientList gameList = monadicIO $ do
+  pre $ not $ null clientList
+  let i' = abs $ mod i $ length clientList
+  let client = clientList !! i'
+  let sid = sessionID client
+
+  clientMVar <- run $ newMVar clientList
+  gameMVar <- run $ newMVar gameList
+
+  run $ Server.Lobby.disconnect clientMVar gameMVar sid
+
+  newClientList <- run $ readMVar clientMVar
+  newGameList <- run $ readMVar gameMVar
+
+  assert $
+    (all (client /=) newClientList) &&
+    (all (\(_,gd) -> all (client /=) $ players gd) newGameList)
