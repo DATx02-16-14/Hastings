6a1746db235b3726566333a09f374fc137b9c010
Kalior <kalioragus@gmail.com>
2016-04-11 11:21:00 +0200

    Add module for testing Server.Lobby

---
+++ test/Server/LobbyTest.hs
@@ -0,0 +1,10 @@
+-- |Contains tests for "Server.Lobby" module.
+module LobbyTest where
+
+import Test.QuickCheck.Monadic
+
+import Server.Lobby
+import ArbitraryLobbyTypes
+
+-- To run in ghci
+-- :l test/Server/LobbyTest.hs src/Hastings/ServerUtils.hs src/Hastings/Utils.hs src/LobbyTypes.hs test/ArbitraryLobbyTypes.hs
