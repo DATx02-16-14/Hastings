c956d224c81e1a6fc5992e011f8ba71fe1c03a60
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-04-15 13:11:01 +0200

    Run tests on one thread to prevent database tests from giving error

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,39 @@
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
+import Test.Framework (defaultMain, defaultMainWithOpts, testGroup)
+import Test.Framework.Options (TestOptions, TestOptions'(..))
+import Test.Framework.Runners.Options (RunnerOptions, RunnerOptions'(..))
+
+
 import Utils
 import Server.LobbyTest
 
-main = defaultMain tests
+
+mainWithOpts = do
+  -- Test options can also be specified in the code. The TestOptions
+  -- type is an instance of the Monoid type class, so the easiest way
+  -- to get an empty set of options is with `mempty`.
+  let empty_test_opts = mempty :: TestOptions
+
+  -- We update the empty TestOptions with our desired values.
+  let my_test_opts = empty_test_opts {
+    topt_maximum_generated_tests = Just 500
+  }
+
+  -- Now we create an empty RunnerOptions in the same way, and add
+  -- our TestOptions to it.
+  let empty_runner_opts = mempty :: RunnerOptions
+  let my_runner_opts = empty_runner_opts {
+    ropt_test_options = Just my_test_opts,
+    -- Run tests on 1 thread only to prevent race conditions on the database.
+    ropt_threads = Just 1
+  }
+
+  defaultMainWithOpts tests my_runner_opts
+
+
+main = mainWithOpts
 
 tests = [
     testGroup "UpdateLookup" [
